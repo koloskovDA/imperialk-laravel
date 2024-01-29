94a59de369ea7b0ef88c8a2b3935bc28766363d9
koloskov.da <koloskov.da@mail.ru>
2024-01-29 15:08:07 +0800

    Enums

---
+++ app/Enums/AuctionStatusEnum.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Enums;
+
+use Spatie\Enum\Enum;
+
+/**
+ * @method static self hidden()
+ * @method static self published()
+ * @method static self finished()
+ */
+class AuctionStatusEnum extends Enum
+{
+}
---
+++ app/Enums/UserStatusEnum.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Enums;
+
+use Spatie\Enum\Enum;
+
+/**
+ * @method static self active()
+ * @method static self blocked()
+ */
+class UserStatusEnum extends Enum
+{
+
+}
