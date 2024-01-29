f0d60e1e18d541bef8338e8681ad256be9c88456
koloskov.da <koloskov.da@mail.ru>
2024-01-29 15:10:03 +0800

    bulma first attempts

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,50 @@
+<nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+        <a href="https://bulma.io" class="m-0 p-0">
+            <img src="https://imperial-k.ru/img/logo.png" width="auto" height="40">
+        </a>
+
+        <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+        </a>
+    </div>
+
+    <div id="navbarBasicExample" class="navbar-menu">
+        <div class="navbar-start">
+            <a class="navbar-item">
+                Аукционы
+            </a>
+
+            <a class="navbar-item">
+                Магазин
+            </a>
+
+            <div class="navbar-item has-dropdown is-hoverable">
+                <a class="navbar-link">
+                    О нас
+                </a>
+
+                <div class="navbar-dropdown">
+                    <a class="navbar-item">
+                        Контакты
+                    </a>
+                    <a class="navbar-item">
+                        Филиалы
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div class="navbar-end">
+            <div class="navbar-item">
+                <div class="buttons">
+                    <a class="button is-link">
+                        <strong>Войти</strong>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</nav>
