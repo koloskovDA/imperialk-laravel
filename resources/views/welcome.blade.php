4296e8b52eedc39bedec690eaffe09b3413d2ffc
koloskov.da <koloskov.da@mail.ru>
2024-02-27 18:34:53 +0800

    welcome.blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,362 @@
 @extends('layouts.main')
 @section('content')
-    <section class="section">
-        <div class="container">
-            <h1 class="title">
-                Hello World
-            </h1>
-            <p class="subtitle">
-                My first website with <strong>Bulma</strong>!
-            </p>
+    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel" style="margin-top: 5px;">
+        <div class="carousel-inner">
+
+
+            <div class="item">
+                <img alt="First slide" src="/img/img1.jpg">
+                <div class="carousel-caption">
+                    <h3>Интернет-аукцион</h3>
+                </div>
+            </div>
+            <div class="item">
+                <img alt="First slide" src="/img/img2.jpg">
+                <div class="carousel-caption">
+                    <h3>Нумизматический салон</h3>
+                </div>
+            </div>
+
+            <div class="item active">
+                <img alt="First slide" src="/img/img3.jpg">
+                <div class="carousel-caption">
+                    <h3>Интернет-магазин</h3>
+                </div>
+            </div>
+
         </div>
-    </section>
+        <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left fa fa-chevron-left"></span>
+        </a>
+        <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right fa fa-chevron-right"></span>
+        </a>
+    </div>
+    <div class="heading heading-v1 margin-bottom-40">
+        <h2>Выставленные лоты</h2>
+    </div>
+    <div class="row margin-bottom-30">
+        <div class="col-md-3 col-sm-6">
+            <div class="product-lots" style="margin-bottom:10px;border: 1px solid #DDDDDD;border-bottom: 1px solid #DDDDDD;text-align: center;">
+                <div class="product-img">
+                    <a href="/auctions/lots/9247">
+                        <img src="/cache/lots/9247/127_1271708436668_image.jpg">
+
+                        <img src="/cache/lots/9247/127_1271708436668_backimage.jpg">
+                        <!--            <img class="full-width img-responsive" src="--><!--" style="max-height: 253px;" alt=""></a>-->
+                    </a>
+                </div>
+                <div class="product-description product-description-brd">
+                    <div class="overflow-h margin-bottom-5">
+                        <h4 class="title-price" style="font-size: 14px;">
+                            <a href="/auctions/lots/9247">10 рублей</a>
+                        </h4>
+
+                    </div>
+
+
+
+                    <div class="product-price">
+
+                        <span class="title-price"></span>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-3 col-sm-6">
+            <div class="product-lots" style="margin-bottom:10px;border: 1px solid #DDDDDD;border-bottom: 1px solid #DDDDDD;text-align: center;">
+                <div class="product-img">
+                    <a href="/auctions/lots/9206">
+                        <img src="/cache/lots/9206/127_1271708417758_image.jpg">
+
+                        <img src="/cache/lots/9206/127_1271708417758_backimage.jpg">
+                        <!--            <img class="full-width img-responsive" src="--><!--" style="max-height: 253px;" alt=""></a>-->
+                    </a>
+                </div>
+                <div class="product-description product-description-brd">
+                    <div class="overflow-h margin-bottom-5">
+                        <h4 class="title-price" style="font-size: 14px;">
+                            <a href="/auctions/lots/9206">3 копейки</a>
+                        </h4>
+
+                    </div>
+
+
+
+                    <div class="product-price">
+
+                        <span class="title-price"></span>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-3 col-sm-6">
+            <div class="product-lots" style="margin-bottom:10px;border: 1px solid #DDDDDD;border-bottom: 1px solid #DDDDDD;text-align: center;">
+                <div class="product-img">
+                    <a href="/auctions/lots/9200">
+                        <img src="/cache/lots/9200/127_1271708416611_image.jpg">
+
+                        <img src="/cache/lots/9200/127_1271708416611_backimage.jpg">
+                        <!--            <img class="full-width img-responsive" src="--><!--" style="max-height: 253px;" alt=""></a>-->
+                    </a>
+                </div>
+                <div class="product-description product-description-brd">
+                    <div class="overflow-h margin-bottom-5">
+                        <h4 class="title-price" style="font-size: 14px;">
+                            <a href="/auctions/lots/9200">2 копейки</a>
+                        </h4>
+
+                    </div>
+
+
+
+                    <div class="product-price">
+
+                        <span class="title-price"></span>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-3 col-sm-6">
+            <div class="product-lots" style="margin-bottom:10px;border: 1px solid #DDDDDD;border-bottom: 1px solid #DDDDDD;text-align: center;">
+                <div class="product-img">
+                    <a href="/auctions/lots/9196">
+                        <img src="/cache/lots/9196/127_1271708416368_image.jpg">
+
+                        <img src="/cache/lots/9196/127_1271708416368_backimage.jpg">
+                        <!--            <img class="full-width img-responsive" src="--><!--" style="max-height: 253px;" alt=""></a>-->
+                    </a>
+                </div>
+                <div class="product-description product-description-brd">
+                    <div class="overflow-h margin-bottom-5">
+                        <h4 class="title-price" style="font-size: 14px;">
+                            <a href="/auctions/lots/9196">25 копеек </a>
+                        </h4>
+
+                    </div>
+
+
+
+                    <div class="product-price">
+
+                        <span class="title-price"></span>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-3 col-sm-6">
+            <div class="product-lots" style="margin-bottom:10px;border: 1px solid #DDDDDD;border-bottom: 1px solid #DDDDDD;text-align: center;">
+                <div class="product-img">
+                    <a href="/auctions/lots/9193">
+                        <img src="/cache/lots/9193/127_1271708416176_image.jpg">
+
+                        <img src="/cache/lots/9193/127_1271708416176_backimage.jpg">
+                        <!--            <img class="full-width img-responsive" src="--><!--" style="max-height: 253px;" alt=""></a>-->
+                    </a>
+                </div>
+                <div class="product-description product-description-brd">
+                    <div class="overflow-h margin-bottom-5">
+                        <h4 class="title-price" style="font-size: 14px;">
+                            <a href="/auctions/lots/9193">25 копеек</a>
+                        </h4>
+
+                    </div>
+
+
+
+                    <div class="product-price">
+
+                        <span class="title-price"></span>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-3 col-sm-6">
+            <div class="product-lots" style="margin-bottom:10px;border: 1px solid #DDDDDD;border-bottom: 1px solid #DDDDDD;text-align: center;">
+                <div class="product-img">
+                    <a href="/auctions/lots/9190">
+                        <img src="/cache/lots/9190/127_1271708415987_image.jpg">
+
+                        <img src="/cache/lots/9190/127_1271708415987_backimage.jpg">
+                        <!--            <img class="full-width img-responsive" src="--><!--" style="max-height: 253px;" alt=""></a>-->
+                    </a>
+                </div>
+                <div class="product-description product-description-brd">
+                    <div class="overflow-h margin-bottom-5">
+                        <h4 class="title-price" style="font-size: 14px;">
+                            <a href="/auctions/lots/9190">10 копеек</a>
+                        </h4>
+
+                    </div>
+
+
+
+                    <div class="product-price">
+
+                        <span class="title-price"></span>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-3 col-sm-6">
+            <div class="product-lots" style="margin-bottom:10px;border: 1px solid #DDDDDD;border-bottom: 1px solid #DDDDDD;text-align: center;">
+                <div class="product-img">
+                    <a href="/auctions/lots/9157">
+                        <img src="/cache/lots/9157/127_1271708265930_image.jpg">
+
+                        <img src="/cache/lots/9157/127_1271708265930_backimage.jpg">
+                        <!--            <img class="full-width img-responsive" src="--><!--" style="max-height: 253px;" alt=""></a>-->
+                    </a>
+                </div>
+                <div class="product-description product-description-brd">
+                    <div class="overflow-h margin-bottom-5">
+                        <h4 class="title-price" style="font-size: 14px;">
+                            <a href="/auctions/lots/9157">15 копеек</a>
+                        </h4>
+
+                    </div>
+
+
+
+                    <div class="product-price">
+
+                        <span class="title-price"></span>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-3 col-sm-6">
+            <div class="product-lots" style="margin-bottom:10px;border: 1px solid #DDDDDD;border-bottom: 1px solid #DDDDDD;text-align: center;">
+                <div class="product-img">
+                    <a href="/auctions/lots/9127">
+                        <img src="/cache/lots/9127/127_1271708239536_image.jpg">
+
+                        <img src="/cache/lots/9127/127_1271708239536_backimage.jpg">
+                        <!--            <img class="full-width img-responsive" src="--><!--" style="max-height: 253px;" alt=""></a>-->
+                    </a>
+                </div>
+                <div class="product-description product-description-brd">
+                    <div class="overflow-h margin-bottom-5">
+                        <h4 class="title-price" style="font-size: 14px;">
+                            <a href="/auctions/lots/9127">20 копеек</a>
+                        </h4>
+
+                    </div>
+
+
+
+                    <div class="product-price">
+
+                        <span class="title-price"></span>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <div class="clearfix"></div>
+    <div class="heading heading-v1 margin-bottom-40">
+        <h2>Последние товары</h2>
+    </div>
+    <div class="row margin-bottom-30">
+        <div class="col-md-4 col-sm-6 md-margin-bottom-30">
+            <div class="product">
+                <div class="product-img">
+                    <a href="https://shop.imperial-k.ru/product/167">
+                        <img class="full-width img-responsive" src="http://imperial-k.ru//cache/product/167/333_1651623217063_image.jpg" alt=""></a>
+
+                </div>
+                <div class="product-description product-description-brd">
+                    <div class="overflow-h margin-bottom-5">
+
+                        <h4 class="title-price">
+                            <a href="https://shop.imperial-k.ru/product/167">10 рублей 2020 г. "Человек труда - Работник транспортной сферы"</a>
+                        </h4>
+
+                        <div class="price">
+
+                            <span> руб.</span>
+                        </div>
+
+
+                    </div>
+                    <div class="product-price">
+                        <span class="title-price"></span>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 col-sm-6 md-margin-bottom-30">
+            <div class="product">
+                <div class="product-img">
+                    <a href="https://shop.imperial-k.ru/product/166">
+                        <img class="full-width img-responsive" src="http://imperial-k.ru//cache/product/166/333_1651623216250_image.jpg" alt=""></a>
+
+                </div>
+                <div class="product-description product-description-brd">
+                    <div class="overflow-h margin-bottom-5">
+
+                        <h4 class="title-price">
+                            <a href="https://shop.imperial-k.ru/product/166">10 рублей 2020 г. "Человек труда - Работник металлургической промышленности"</a>
+                        </h4>
+
+                        <div class="price">
+
+                            <span> руб.</span>
+                        </div>
+
+
+                    </div>
+                    <div class="product-price">
+                        <span class="title-price"></span>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 col-sm-6 md-margin-bottom-30">
+            <div class="product">
+                <div class="product-img">
+                    <a href="https://shop.imperial-k.ru/product/163">
+                        <img class="full-width img-responsive" src="http://imperial-k.ru//cache/product/163/333_1651577404029_image.jpg" alt=""></a>
+
+                </div>
+                <div class="product-description product-description-brd">
+                    <div class="overflow-h margin-bottom-5">
+
+                        <h4 class="title-price">
+                            <a href="https://shop.imperial-k.ru/product/163">10 рублей биметалл 2019 г. «75-летие Победы советского народа в Великой Отечественной войне»</a>
+                        </h4>
+
+                        <div class="price">
+
+                            <span> руб.</span>
+                        </div>
+
+
+                    </div>
+                    <div class="product-price">
+                        <span class="title-price"></span>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
