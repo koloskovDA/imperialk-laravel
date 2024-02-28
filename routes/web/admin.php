078a23e68327664730d280ab0d67bf180279f5f7
koloskov.da <koloskov.da@mail.ru>
2024-02-28 22:44:48 +0800

    route web admin

--- routes/web/admin.php
+++ routes/web/admin.php
@@ -1,1 +1,9 @@
 <?php
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\IndexController;
+
+Route::prefix('admin')->group(function() {
+    Route::get('/', [IndexController::class, 'index'])->name('index');
+    Route::resource('auctions', \App\Http\Controllers\Admin\AuctionController::class);
+
+});
