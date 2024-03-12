3b7f331b4e837327145de6613847f7ea663305fe
koloskov.da <koloskov.da@mail.ru>
2024-03-12 18:01:34 +0800

    AuctionController show()

--- app/Http/Controllers/Admin/AuctionController.php
+++ app/Http/Controllers/Admin/AuctionController.php
@@ -3,12 +3,20 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Auction;
 
 class AuctionController extends Controller
 {
     public function list()
     {
         return view('admin.auctions.list');
+    }
+
+    public function show($auctionId)
+    {
+        $auction = Auction::find($auctionId)->with('lots')->first();
+
+        return view('admin.auctions.show', ['auction' => $auction]);
     }
 
 }
