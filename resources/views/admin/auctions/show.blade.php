246630b8fe0ee576fc6cc7390cbe096ff780ef9e
koloskov.da <koloskov.da@mail.ru>
2024-03-12 18:03:34 +0800

    admin.auctions.show.blade

---
+++ resources/views/admin/auctions/show.blade.php
@@ -0,0 +1,12 @@
+@extends('admin.index')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Лоты аукциона {{$auction->name}}</h1>
+@stop
+
+@section('content')
+    @livewire('admin.lots', ['auction' => $auction])
+@stop
+нфкт
