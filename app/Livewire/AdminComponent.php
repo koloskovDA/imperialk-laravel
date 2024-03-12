09dd78c824a7779d013ef112a1c3ce294e0f585e
koloskov.da <koloskov.da@mail.ru>
2024-03-13 01:53:45 +0800

    lw AdminComponent

---
+++ app/Livewire/AdminComponent.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\Auction;
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Collection;
+use Livewire\Component;
+use Livewire\WithPagination;
+
+class AdminComponent extends Component
+{
+    use WithPagination;
+
+    protected $paginationTheme = 'bootstrap';
+
+    public array $properties = [];
+    public array $requiredProperties = [];
+    public array $propertiesLabels = [];
+    public array $values = [];
+
+    public string $class = '';
+    public string $classLabel = '';
+
+    public Model|null $toUpdate = null;
+    public Model|null $toDelete = null;
+
+    public string $createUrl = '';
+    public string $editUrl = '';
+
+    public array $rules = [];
+    public array $rulesMessages = [];
+
+    public array $inputTypes = [];
+
+    public array $enums = [];
+
+    public array $search = [];
+
+    public int $paginator = 2;
+
+    public function render()
+    {
+        $collection = call_user_func($this->class .'::query');
+
+        foreach ($this->search as $prop => $value)
+        {
+            if ($value != null)
+            {
+                $collection = $collection->where($prop, 'ilike', '%'.$value.'%');
+            }
+        }
+        if ($this->paginator > 0)
+        {
+            $collection = $collection->paginate($this->paginator);
+        }
+        else
+        {
+            $collection = $collection->get();
+        }
+
+        return view('livewire.admin.admin-component', compact('collection'));
+    }
+
+    public function createInstance()
+    {
+
+    }
+
+    public function confirmCreate()
+    {
+        $this->validate($this->rules, $this->rulesMessages);
+        call_user_func($this->class .'::create', $this->values);
+        $this->values = [];
+
+        $this->dispatch('close-modal');
+    }
+
+    public function editInstance(array $model)
+    {
+        $this->values = array_flip($this->properties);
+        $this->toUpdate = call_user_func($this->class .'::find', $model['id']);
+        $instanceAttributes = array_intersect_key($this->toUpdate->getAttributes(), $this->values);
+
+        $this->values = $instanceAttributes;
+    }
+
+    public function confirmEdit()
+    {
+        $this->validate($this->rules, $this->rulesMessages);
+        $this->toUpdate->update($this->values);
+        $this->values = [];
+
+        $this->dispatch('close-modal');
+    }
+
+    public function deleteInstance(array $model)
+    {
+        $this->toDelete = call_user_func($this->class .'::find', $model['id']);
+    }
+
+    public function confirmDelete()
+    {
+        if ($this->toDelete)
+        {
+            $this->toDelete->delete();
+        }
+        $this->toDelete = null;
+    }
+}
---
+++ resources/views/livewire/admin/admin-component.blade.php
@@ -0,0 +1,175 @@
+<div>
+    <div class="card">
+        <div class="card-header">
+            <button type="button" wire:click="createInstance" class="btn btn-success" data-toggle="modal" data-target="#modal-default">
+                Создать {{strtolower($classLabel)}}
+            </button>
+        </div>
+
+        <div class="card-body">
+            <div id="example2_wrapper" class="dataTables_wrapper dt-bootstrap4">
+                <div class="row">
+                    <div class="col-sm-12 col-md-6">
+
+                    </div>
+                    <div class="col-sm-12 col-md-6">
+
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-12">
+                        <table id="example2" class="table table-bordered table-hover dataTable dtr-inline" aria-describedby="example2_info">
+                            <thead>
+                            <tr>
+                                @foreach($propertiesLabels as $label)
+                                    <th class="sorting sorting_asc" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-sort="ascending" aria-label="Rendering engine: activate to sort column descending">
+                                        {{$label}}
+                                    </th>
+                                @endforeach
+                                <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="CSS grade: activate to sort column ascending">
+
+                                </th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr class="odd">
+                                @foreach($properties as $label)
+                                    <td class="dtr-control sorting_1" tabindex="0">
+                                        @if(array_key_exists($label, $search))
+                                            <input class="form-control" type="search" wire:model.live="search.{{$label}}" placeholder="Поиск" aria-label="Поиск">
+                                        @endif
+                                    </td>
+                                @endforeach
+                                <td>
+                                </td>
+                            </tr>
+                            @foreach($collection as $item)
+                                <tr class="odd">
+                                    @foreach($properties as $property)
+                                        <td class="dtr-control sorting_1" tabindex="0">
+                                            {{ $item->{$property} }}
+                                        </td>
+                                    @endforeach
+                                    <td>
+                                        <button class="btn btn-sm btn-light"><i class="far fa-fw fa-eye"></i></button>
+                                        <button class="btn btn-sm btn-light" data-toggle="modal" data-target="#modal-edit" wire:click="editInstance({{$item}})">
+                                            <i class="fas fa-fw fa-pencil"></i>
+                                        </button>
+                                        <button class="btn btn-sm btn-light" data-toggle="modal" data-target="#modal-sm" wire:click="deleteInstance({{$item}})">
+                                            <i class="far fa-fw fa-trash-can"></i>
+                                        </button>
+                                    </td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-12 col-md-5">
+                    </div>
+                    <div class="col-sm-12 col-md-7">
+                        <div class="dataTables_paginate paging_simple_numbers" id="example2_paginate">
+                            {{ $collection->links() }}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="modal fade" wire:ignore.self id="modal-default" style="display: none;" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title">Создать {{$classLabel}}</h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    @foreach($inputTypes as $key => $inputType)
+                        @if ($inputType !== 'select')
+                            <div class="form-group">
+                                <label for="">{{$propertiesLabels[$key]}}</label>
+                                <input class="form-control" wire:model="values.{{$key}}" type="{{$inputType}}">
+                                @error('values.'.$key) <span class="error">{{ $message }}</span> @enderror
+                            </div>
+                        @else
+                            <div class="form-group">
+                                <label for="">{{$propertiesLabels[$key]}}</label>
+                                <select wire:model="values.{{$key}}" class="custom-select">
+                                    @foreach($enums[$key] as $enumKey => $enumValue)
+                                        <option value="{{$enumKey}}">{{$enumValue}}</option>
+                                    @endforeach
+                                </select>
+                                @error('values.'.$key) <span class="error">{{ $message }}</span> @enderror
+                            </div>
+                        @endif
+                    @endforeach
+                </div>
+                <div class="modal-footer justify-content-between">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Закрыть</button>
+                    <button type="button" class="btn btn-primary" wire:click="confirmCreate">Сохранить</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="modal fade" wire:ignore.self id="modal-edit" style="display: none;" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title">Изменить {{$classLabel}}</h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    @foreach($inputTypes as $key => $inputType)
+                        @if ($inputType !== 'select')
+                            <div class="form-group">
+                                <label for="">{{$propertiesLabels[$key]}}</label>
+                                <input class="form-control" wire:model="values.{{$key}}" type="{{$inputType}}">
+                                @error('values.'.$key) <span class="error">{{ $message }}</span> @enderror
+                            </div>
+                        @else
+                            <div class="form-group">
+                                <label for="">{{$propertiesLabels[$key]}}</label>
+                                <select wire:model="values.{{$key}}" class="custom-select">
+                                    @foreach($enums[$key] as $enumKey => $enumValue)
+                                        <option value="{{$enumKey}}">{{$enumValue}}</option>
+                                    @endforeach
+                                </select>
+                                @error('values.'.$key) <span class="error">{{ $message }}</span> @enderror
+                            </div>
+                        @endif
+                    @endforeach
+                </div>
+                <div class="modal-footer justify-content-between">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Закрыть</button>
+                    <button type="button" class="btn btn-primary" wire:click="confirmEdit">Сохранить</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="modal fade" wire:ignore.self id="modal-sm" style="display: none;" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title">Удалить {{strtolower($classLabel)}}</h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <p>Вы действительно хотите удалить {{$classLabel}}?</p>
+                </div>
+                <div class="modal-footer justify-content-between">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Закрыть</button>
+                    <button type="button" class="btn btn-danger" data-dismiss="modal" wire:click="confirmDelete">Удалить</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
